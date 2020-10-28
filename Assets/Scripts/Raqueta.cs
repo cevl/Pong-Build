a1e7e9d36d8790352669070a13500cdf8db6626f
Cesar Emilio <cevl960504@gmail.com>
2020-10-28 09:05:05 -0400

    Entrega

---
+++ Assets/Scripts/Raqueta.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raqueta : MonoBehaviour {
+    //Velocidad
+    public float velocidad = 30.0f;
+    //Eje vertical
+    public string eje;
+    public string ejeHorizontal;
+
+    // Es llamado una vez cada fixed frame
+    void FixedUpdate () {
+        //Capto el valor del eje vertical de la raqueta
+        float v = Input.GetAxisRaw(eje);
+        //Capto el valor del eje horizontal de la raqueta
+        float h = Input.GetAxisRaw(ejeHorizontal);
+        //Modifico la velocidad de la raqueta
+        GetComponent<Rigidbody2D>().velocity = new Vector2(h * velocidad, v * velocidad);
+        }
+}
